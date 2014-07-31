ed564605e08b129c31c56670e6e1a028db6b9b71
Jan Menšík <jan@mensik.cz>
2014-07-31 16:52:35 +0200

    FIX: fuction.parsedate
    Bug with 00 (0 - midnight) hours wrongly changed to 12 (noon).

--- function.parsedate.php
+++ function.parsedate.php
@@ -17,10 +17,10 @@ function parsedate ($data, $force = false) {
 		$output = $datum[1];
 	# 21. [0]9. 2013 [11:30.59]
 	elseif (preg_match ('/([0-9]{1,2})\. ?([0-9]{1,2})\. ?([1-9][0-9]{3})( ?-? *([0-9]{1,2}):([0-9]{1,2})([:.]([0-9]{1,2}))?)?/', $data, $datum))
-		$output = mktime ($datum[5] ? $datum[5] : 12, $datum[6] ? $datum[6] : 0, $datum[8] ? $datum[8] : 0, $datum[2], $datum[1], $datum[3]);
+		$output = mktime (isset ($datum[5]) ? $datum[5] : 12, $datum[6] ? $datum[6] : 0, $datum[8] ? $datum[8] : 0, $datum[2], $datum[1], $datum[3]);
 	# 21. [0]9. [11:30.59]  - bez roku
 	elseif (preg_match ('/([0-9]{1,2})\. ?([0-9]{1,2})\. ?( ?-? *([0-9]{1,2}):([0-9]{1,2})([:.]([0-9]{1,2}))?)?/', $data, $datum2)) {
-		$output = mktime ($datum2[4] ? $datum2[4] : 12, $datum2[5] ? $datum2[5] : 0, $datum2[7] ? $datum2[7] : 0, $datum2[2], $datum2[1]);
+		$output = mktime (isset ($datum2[4]) ? $datum2[4] : 12, $datum2[5] ? $datum2[5] : 0, $datum2[7] ? $datum2[7] : 0, $datum2[2], $datum2[1]);
 		//print_r ($data);
 		}
 	elseif ($force)
